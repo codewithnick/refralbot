9ba3fe0864970bc2c36b54fe93784af76c606cd7
Nnamdi <nnamdiibns@gmail.com>
2018-05-28 14:50:59 +0100

    Add new message to be sent

--- bot/bot_router.py
+++ bot/bot_router.py
@@ -1,10 +1,10 @@
-# import json
-# import time
-# from django.http import HttpResponse
-# from django.conf import settings
+import json
+import time
+from django.http import HttpResponse
+from django.conf import settings
 import telepot
-# from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ForceReply
-# from bot.models import Person, Referral, Setting, Bot
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ForceReply
+from bot.models import Person, Referral, Setting, Bot
 import urllib3
 
 proxy_url = "http://proxy.server:3128"
@@ -27,22 +27,25 @@ telepot.api._onetime_pool_spec = (
         timeout=30
     )
 )
-# settings = Setting.objects.get()
-# specs = Bot.objects.get()
+config = Setting.objects.get()
+specs = Bot.objects.get()
 
-# bot = telepot.bot(specs.api_key)
+bot = telepot.bot(specs.api_key)
+
 # Secret GUID number to hide our endpoint
 SECRET = "b6f2a100-6263-11e8-adc0-fa7ae01bbebc"
 
 # Our URL to serve as the webhook for Telegram
 END_POINT = 'bappa.pythonanywhere.com/bot/prod/hook/{}/'.format(SECRET)
-bot = telepot.Bot('486245389:AAFwqUcArzLWJsvopZh4lgPll9RU8vVW57M')
+
+# bot = telepot.Bot('486245389:AAFwqUcArzLWJsvopZh4lgPll9RU8vVW57M')
+
 webhook = bot.getWebhookInfo()
 if not webhook['url']:
     print('No WEbhook set')
-    # bot.setWebhook(url=settings.END_POINT)
-    bot.setWebhook(url=END_POINT)
-    print('SEt webhook: {}'.format(END_POINT))
+    bot.setWebhook(url=settings.END_POINT)
+    # bot.setWebhook(url=END_POINT)
+    print('SEt webhook: {}'.format(settings.END_POINT))
 else:
     print('Webhook already set.')
 print(bot.getWebhookInfo())
@@ -55,3 +58,7 @@ def route(msg):
 
     if content_type == 'text':
         bot.sendMessage(chat_id, msg['text'])
+        bot.sendMessage(
+            chat_id,
+            'The end point is {}'.format(settings.END_POINT)
+        )
