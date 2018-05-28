7f4f4c170243606cb16bc517f9aa2003cf0a3b54
Nnamdi <nnamdiibns@gmail.com>
2018-05-28 14:40:28 +0100

    Access message dict

--- bot/bot_router.py
+++ bot/bot_router.py
@@ -7,26 +7,26 @@ import telepot
 # from bot.models import Person, Referral, Setting, Bot
 import urllib3
 
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(
-        proxy_url=proxy_url,
-        num_pools=3,
-        maxsize=10,
-        retries=False,
-        timeout=30
-    ),
-}
-telepot.api._onetime_pool_spec = (
-    urllib3.ProxyManager,
-    dict(
-        proxy_url=proxy_url,
-        num_pools=1,
-        maxsize=1,
-        retries=False,
-        timeout=30
-    )
-)
+# proxy_url = "http://proxy.server:3128"
+# telepot.api._pools = {
+#     'default': urllib3.ProxyManager(
+#         proxy_url=proxy_url,
+#         num_pools=3,
+#         maxsize=10,
+#         retries=False,
+#         timeout=30
+#     ),
+# }
+# telepot.api._onetime_pool_spec = (
+#     urllib3.ProxyManager,
+#     dict(
+#         proxy_url=proxy_url,
+#         num_pools=1,
+#         maxsize=1,
+#         retries=False,
+#         timeout=30
+#     )
+# )
 # settings = Setting.objects.get()
 # specs = Bot.objects.get()
 
@@ -35,7 +35,7 @@ telepot.api._onetime_pool_spec = (
 SECRET = "b6f2a100-6263-11e8-adc0-fa7ae01bbebc"
 
 # Our URL to serve as the webhook for Telegram
-END_POINT = 'bappa.pythonanywhere.com/bot/prod/hook/{}'.format(SECRET)
+END_POINT = 'bappa.pythonanywhere.com/bot/prod/hook/{}/'.format(SECRET)
 bot = telepot.Bot('486245389:AAFwqUcArzLWJsvopZh4lgPll9RU8vVW57M')
 webhook = bot.getWebhookInfo()
 if not webhook['url']:
@@ -45,10 +45,11 @@ if not webhook['url']:
     print('SEt webhook: {}'.format(END_POINT))
 else:
     print('Webhook already set.')
-# print(bot.getWebhookInfo())
+print(bot.getWebhookInfo())
 
 
 def route(msg):
+    msg = msg['message']
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
