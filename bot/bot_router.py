718e6db75c364f461fb4221b305d137b9ae75f49
Nnamdi <nnamdiibns@gmail.com>
2018-05-28 14:32:08 +0100

    Add code to enable PA account reach telegram servers

--- bot/bot_router.py
+++ bot/bot_router.py
@@ -5,7 +5,28 @@
 import telepot
 # from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ForceReply
 # from bot.models import Person, Referral, Setting, Bot
+import urllib3
 
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(
+        proxy_url=proxy_url,
+        num_pools=3,
+        maxsize=10,
+        retries=False,
+        timeout=30
+    ),
+}
+telepot.api._onetime_pool_spec = (
+    urllib3.ProxyManager,
+    dict(
+        proxy_url=proxy_url,
+        num_pools=1,
+        maxsize=1,
+        retries=False,
+        timeout=30
+    )
+)
 # settings = Setting.objects.get()
 # specs = Bot.objects.get()
 
