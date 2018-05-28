bc587f8f3d1218067fa4f27e67791b7621dcbb22
Nnamdi <nnamdiibns@gmail.com>
2018-05-28 15:38:13 +0100

    Fix bug

--- bot/models.py
+++ bot/models.py
@@ -53,7 +53,7 @@ class Setting(SingletonModel):
 
 
 class Bot(SingletonModel):
-    api_keys = models.CharField('API Keys', max_length=100)
+    api_key = models.CharField('API Keys', max_length=100)
     name = models.CharField('Bot Name', max_length=50)
     webhook = models.URLField()
     last_updated = models.DateTimeField(auto_now=True)
