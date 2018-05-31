fe936abe3acc278d4759d67f44653469bccf722a
Nnamdi <nnamdiibns@gmail.com>
2018-05-31 15:04:16 +0100

    Fix bug

--- bot/urls.py
+++ bot/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'bot'
 urlpatterns = [
     path('prod/hook/{}/'.format(settings.SECRET), views.hook, name='hook'),
-    path('prod/referall/', views.referral, name='referall_join'),
+    # path('prod/referall/', views.referral, name='referall_join'),
     # path('prod/')
 ]
