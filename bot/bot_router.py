c4fb91996a7c204ee63d5143528ab184c31f8195
Nnamdi <nnamdiibns@gmail.com>
2018-06-06 15:52:47 +0100

    Fix naming error

--- bot/bot_router.py
+++ bot/bot_router.py
@@ -76,7 +76,7 @@ if config and bot_specs:
         except:
             print('Error')
         # bot.setWebhook(url=END_POINT)
-        print('SEt webhook: {}'.format(bot_specs.webhok))
+        print('SEt webhook: {}'.format(bot_specs.webhook))
     else:
         print('Webhook already set.')
     print(bot.getWebhookInfo())
